988a95eb2c9001d770fe904a87d390fb224baaa7
TreWalk <111515307+TreWalk@users.noreply.github.com>
2023-05-09 00:29:19 -0400

    Add files via upload

---
+++ TaskList.py
@@ -0,0 +1,61 @@
+tasks = []
+
+while True:
+    user_choice = input('Type add, show, edit, complete or exit: ')
+    user_choice = user_choice.strip()
+
+    if user_choice == 'add':
+        task = input('Enter a Task: ') + '\n'
+
+        with open('tasklist.txt', 'r') as file:
+            tasks = file.readlines()
+
+        tasks.append(task)
+
+        with open('tasklist.txt', 'w') as file:
+            file.writelines(tasks)
+
+    elif user_choice == 'show':
+        try:
+            with open('tasklist.txt', 'r') as file:
+                tasks = file.readlines()
+            for index, item in enumerate(tasks):
+                if not item.strip():
+                    continue
+            row = f"{index + 1}.{item.strip().title()}"
+            print(row)
+        except FileNotFoundError:
+            print('Error: File not found')
+
+    elif user_choice == 'edit':
+        number = int(input('Item Number to Edit: '))
+        number = number -1
+
+        with open('tasklist.txt', 'r') as file:
+            tasks = file.readlines()
+
+        new_task = input('Enter New Task: ')
+        tasks[number] = new_task + '\n'
+
+        with open("tasklist.txt", 'w') as file:
+            file.writelines(tasks)
+
+    elif user_choice == 'complete':
+        number = int(input('Task Number to Complete: '))
+
+        with open('tasklist.txt', 'r') as file:
+            tasks = file.readlines()
+        index = number -1
+        completed_task = tasks[index].strip('\n')
+        tasks.pop(index)
+
+        with open('tasklist.txt', 'w') as file:
+            file.writelines(tasks)
+
+        print(f'---Task {number}: {completed_task.strip()} has been completed.---')
+
+    elif user_choice == 'exit':
+        print('---Program successfully closed---')
+        break
+    else:
+        print('Invalid Command')
